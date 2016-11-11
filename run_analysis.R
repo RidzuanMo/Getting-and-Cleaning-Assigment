4214c6e4c2e5c5e7cade5211c91bd221772eb26a
Ridzuan Mohamad <ridzuanmo@gmail.com>
2016-11-11 22:59:04 +0800

    merge test and train dataset as tidy dataset

--- run_analysis.R
+++ run_analysis.R
@@ -22,6 +22,11 @@ features <- fread(file.path(DATASET_HOME, "features.txt")) %>%
 
 colnames(features) <- c("featureId", "feature")
 
+
+#
+# Load, read and construct train dataset
+#
+
 train_data <- fread(file.path(DATASET_HOME, "train", "X_train.txt")) %>%
     select(features[,"featureId"])
 colnames(train_data) <- features[,"feature"]
@@ -32,6 +37,32 @@ colnames(train_activity) <- c("activityId")
 train_subject <- fread(file.path(DATASET_HOME, "train", "subject_train.txt"))
 colnames(train_subject) <- c("subjectId")
 
+train_dataset <- cbind(train_subject, train_activity, train_data)
+
+#
+# Load, read and construct test dataset
+#
+
+test_data <- fread(file.path(DATASET_HOME, "test", "X_test.txt")) %>%
+    select(features[,"featureId"])
+colnames(test_data) <- features[,"feature"]
+
+test_activity <- fread(file.path(DATASET_HOME, "test", "y_test.txt"))
+colnames(test_activity) <- c("activityId")
+
+test_subject <- fread(file.path(DATASET_HOME, "test", "subject_test.txt"))
+colnames(test_subject) <- c("subjectId")
+
+test_dataset <- cbind(test_subject, test_activity, test_data)
 
 
+#
+# Merge both dataset as final tidy dataset
+#
+tidy_dataset <- rbind(train_dataset, test_dataset)
+
+#
+# write the tidy dataset into file "tidy_dataset.txt"
+#
+write.table(tidy_dataset, file.path("tidy_dataset.txt"), row.names=FALSE)
 
